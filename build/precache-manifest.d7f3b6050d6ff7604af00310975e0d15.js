795f22335a125cee600f9408c246e78d83d37c87
Daniel Donaldson <donaldson.daniel@gmail.com>
2020-08-10 15:13:37 -0500

    Add files via upload
    Initial upload to GitHub from local env

---
+++ build/precache-manifest.d7f3b6050d6ff7604af00310975e0d15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c749c711f8f0ff4f1eda8631612342d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9011b459e55576a08167",
+    "url": "/static/css/main.a9d6d307.chunk.css"
+  },
+  {
+    "revision": "16dc095645b2efdbf054",
+    "url": "/static/js/2.6046a761.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6046a761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9011b459e55576a08167",
+    "url": "/static/js/main.1d3acd16.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
